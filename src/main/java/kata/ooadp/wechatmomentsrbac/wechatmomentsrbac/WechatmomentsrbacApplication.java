1ee98c9b67f790d93b83593846bf1a0e7358e256
Qian <wubin28@outlook.com>
2022-11-27 00:28:02 +0800

    chore: make a blank project using start.spring.io

---
+++ src/main/java/kata/ooadp/wechatmomentsrbac/wechatmomentsrbac/WechatmomentsrbacApplication.java
@@ -0,0 +1,13 @@
+package kata.ooadp.wechatmomentsrbac.wechatmomentsrbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatmomentsrbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatmomentsrbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/ooadp/wechatmomentsrbac/wechatmomentsrbac/WechatmomentsrbacApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.ooadp.wechatmomentsrbac.wechatmomentsrbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatmomentsrbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
