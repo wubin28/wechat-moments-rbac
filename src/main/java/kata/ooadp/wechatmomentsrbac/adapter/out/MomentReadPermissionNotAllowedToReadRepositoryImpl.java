827a07840bee1522e177a0f63ea7aaa1587cb8dc
Qian <wubin28@outlook.com>
2022-11-27 11:22:54 +0800

    fix: should return the friend who is not allowed to read zhao's moments

--- src/main/java/kata/ooadp/wechatmomentsrbac/adapter/out/MomentReadPermissionNotAllowedToReadRepositoryImpl.java
+++ src/main/java/kata/ooadp/wechatmomentsrbac/adapter/out/MomentReadPermissionNotAllowedToReadRepositoryImpl.java
@@ -7,11 +7,20 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class MomentReadPermissionNotAllowedToReadRepositoryImpl implements MomentReadPermissionNotAllowedToReadRepository {
+
+    private final Friends friends;
+
+    public MomentReadPermissionNotAllowedToReadRepositoryImpl() {
+        this.friends = new Friends();
+        friends.add(new Friend("zhao", "qian"));
+        friends.add(new Friend("zhao", "sun"));
+        friends.add(new Friend("zhao", "li"));
+    }
+
     @Override
     public Friends findAllFriendsWhoAreNotAllowedToReadMyMoments(String userAccount) {
         Friends result = new Friends();
-        result.add(new Friend("zhao", "sun"));
-        result.add(new Friend("zhao", "li"));
+        result.add(new Friend(friends.get(0).getMe().getUserAccount(), friends.get(0).getFriend().getUserAccount()));
         return result;
     }
 }
